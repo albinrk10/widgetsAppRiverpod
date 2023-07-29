6028d08cd42626ad543beacd8f08901de62b7a52
albin <amacavilca.abx@gmail.com>
2023-07-29 12:39:46 -0500

    screens creadas

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTutorailScreen extends StatelessWidget {
+  
+  static const name = 'tutorail_screen';
+
+  const AppTutorailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,11 @@
+export 'package:widgets_albin/presentation/screens/animated/animared_screen.dart';
+export 'package:widgets_albin/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_albin/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_albin/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_albin/presentation/screens/home/home_screen.dart';
+export 'package:widgets_albin/presentation/screens/infinite_scroll/inifinite_scroll_screen.dart';
+export 'package:widgets_albin/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_albin/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_albin/presentation/screens/ui_controls/ui_controls_screen.dart';
+
 
