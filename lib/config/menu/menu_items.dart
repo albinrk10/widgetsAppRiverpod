c87c4244e4869d66346b2961978a437f2a8c2068
albin <amacavilca.abx@gmail.com>
2023-07-28 20:31:57 -0500

    Home Screen creado

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
+}
+
+const appMenuIteams = <MenuItem>[ 
+  MenuItem(
+    title: 'Botones',
+    subTitle: 'Varios botones en Flutter',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined
+    ),
+    MenuItem(
+    title: 'Tarjetas',
+    subTitle: 'Un contendor estilizado',
+    link: '/card',
+    icon: Icons.credit_card
+    ),
+];
