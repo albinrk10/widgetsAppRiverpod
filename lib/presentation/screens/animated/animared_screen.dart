769b4d118a362f0b118334d36ef039b3c003f014
albin <amacavilca.abx@gmail.com>
2023-07-30 13:21:23 -0500

    animared terminado

--- lib/presentation/screens/animated/animared_screen.dart
+++ lib/presentation/screens/animated/animared_screen.dart
@@ -1,13 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
-  
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double heigth = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changesShape() {
+    final random = Random();
+
+    width = random.nextInt(300) + 120;
+    heigth = random.nextInt(300) + 120;
+    color = Color.fromARGB(
+        random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+    borderRadius = random.nextInt(100) + 20;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.elasticInOut,
+          width: width <= 0 ? 0 : width,
+          height: heigth <= 0 ? 0 : heigth,
+          decoration: BoxDecoration(
+              color: color, borderRadius: BorderRadius.circular(borderRadius < 0 ? 0 : borderRadius)),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changesShape,
+        child: const Icon(Icons.play_arrow_rounded),
+      ),
+    );
   }
 }
