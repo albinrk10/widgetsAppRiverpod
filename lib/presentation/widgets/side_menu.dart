5a25f0f31c72bc458c5969ec206793b58e9668da
albin <amacavilca.abx@gmail.com>
2023-07-30 19:36:37 -0500

    Navegar desde menu

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -1,30 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_albin/config/menu/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
-  const SideMenu({super.key});
+  final GlobalKey<ScaffoldState> scaffoldKey;
+
+  const SideMenu({super.key, required this.scaffoldKey});
 
   @override
   State<SideMenu> createState() => _SideMenuState();
 }
 
 class _SideMenuState extends State<SideMenu> {
-  int navDraweIndex = 1;
+  int navDraweIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
         selectedIndex: navDraweIndex,
         onDestinationSelected: (value) {
           setState(() {
             navDraweIndex = value;
           });
+
+          final menuItem = appMenuIteams[value];
+          context.push(menuItem.link);
+          widget.scaffoldKey.currentState?.closeDrawer();
         },
-        children: const [
-          NavigationDrawerDestination(
-              icon: Icon(Icons.add), label: Text('Home Screen')),
-          NavigationDrawerDestination(
-              icon: Icon(Icons.add_shopping_cart_rounded),
-              label: Text('Otra Pantalla'))
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20, 16, 10),
+            child: const Text('Main'),
+          ),
+          ...appMenuIteams.sublist(0, 3).map(
+                (item) => NavigationDrawerDestination(
+                    icon: Icon(item.icon), label: Text(item.title)),
+              ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(26, 16, 16, 10),
+            child: Divider(),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(
+              28,
+              10,
+              16,
+              10,
+            ),
+            child: Text('More options'),
+          ),
+          ...appMenuIteams.sublist(3).map(
+                (item) => NavigationDrawerDestination(
+                    icon: Icon(item.icon), label: Text(item.title)),
+              ),
         ]);
   }
 }
